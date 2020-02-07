b26a501885a4e301abcddce5932b3a960c3c6e05
Glitch (team-mate--hackathon) <none>
2020-02-07 21:36:19 +0000

    🌻🏉 Updated with Glitch

---
+++ GameScreen.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  
+let data =[
+  {
+    picture:"",
+    question:"Click the choices?",
+    choices:["b0","b1","b2","b3"],
+    impact:[1,-1,-2,0],
+  },
+  {},
+] 
+  
+  
+  
+  
+  
+  
+  
+});
+
---
+++ TeamMate.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  
+  console.log("ready!");
+  $(".buttonStart").click(function() {
+     window.location.assign("GameScreen.html");
+
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+});
