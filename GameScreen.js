68ff5492e6baba6ecfed83c913bda3985f3fa73d
Glitch (team-mate--hackathon) <none>
2020-02-08 00:25:52 +0000

    💏🎍 Updated with Glitch

--- GameScreen.js
+++ GameScreen.js
@@ -3,72 +3,76 @@ $(document).ready(function() {
 let index = 0
 let data =[
   {
-    picture:"https://via.placeholder.com/350x150",
-    question:"After School you are invited to go eat with the smokers and vapers of the school, you know they have a bad rep but you feel included that they even asked you.",
-    choices:["yes go eat with them","no, don't "],
+    picture:"https://cdn.glitch.com/6bec49b0-c740-4af8-809e-979aa297be0f%2FChipotle.png?v=1581119800868",
+    question:"After School you are invited to go to Chipotle with the smokers and vapers of the school. You know they have a bad rep but you feel included that they even asked you. What do you do?",
+    choices:["Yes, go eat with them","no, don't "],
     impact:[-5,0],
     nextQuestion:[1, 2],
   },
   {
-    picture:"https://via.placeholder.com/350x150",
-    question:"You're out with the group and they pull out cigarettes and vapes, they ask you to hit it",
+    picture:"https://calmatters.org/wp-content/uploads/sites/2/2018/09/41552544744_2ae2d565c6_o-e1536791053851.jpg?fit=794%2C529",
+    question:"You're out with the group and they pull out cigarettes and vapes. Someone passes a vape to you and asks you if you want a hit.",
     choices:["You go for the Juul because you think it's less harmful","You say yes to the Cigarrette because you know its only tobacco","You decline because you now that's not the path you want to take"],
     impact:[-10,-15,3],
     nextQuestion:[3,4,5],
+    //2
   },
   {
-    picture:"https://via.placeholder.com/350x150",
+    picture:"https://cdn.glitch.com/6bec49b0-c740-4af8-809e-979aa297be0f%2FBobaguys.jpeg?v=1581119865917",
     question:"You go to boba instead,there you see a friend and join them, but then they offer you a hit of their vape ",
     choices:["Hesistate when saying No Thanks","Oh well why not, and hit the Juul","Decline nicely because you don't want to do that"],
     impact:[-1,-5,3],
-    nextQuestion:[6,7,8],
+    nextQuestion:[],
   },
   {
-    picture:"https://via.placeholder.com/350x150",
-    question:"Since you hit the juul, you now have a head rush",
-    choices:["Tell them you don't feel well","Ask them if what you're feeling is normal","Ignore it and keep juuling"],
+    picture:"https://cdn.glitch.com/6bec49b0-c740-4af8-809e-979aa297be0f%2FScreenshot%202020-02-07%20at%204.16.35%20PM.png?v=1581121015120",
+    question:"Since you hit the juul, you now have a head rush.",
+    choices:["Ask them if what you're feeling is normal","Ignore it and keep juuling"],
     impact:[3,3,-5],
-    nextQuestion:[9,10,11],
+    nextQuestion:[6,7],
+    //2a
   },
   {
-    picture:"https://via.placeholder.com/350x150",
+    picture:"https://us.123rf.com/450wm/keath/keath1805/keath180500061/100874789-stock-vector-man-coughing-simple-line-style-icon-medical-concept-illustration.jpg?ver=6",
     question:"You begin coughing ucontrollably because of the cigarette ",
     choices:["You leave telling them that you have to go somewhere","Keep taking hits of the cigarette",],
     impact:[3,-5],
-    nextQuestion:[],
+    nextQuestion:[9,10],
+    //2b
   },
   {
-    picture:"https://via.placeholder.com/350x150",
-    question:"They tell you that you're scary and pressure you to pick one to smoke",
-    choices:["Pick either one because you don't want to seem scary","You Run away"],
+    picture:"https://i7.pngguru.com/preview/21/937/100/peer-pressure-person-conformity-minority-group-value-keji.jpg",
+    question:"They tell you that you're scared and pressure you to pick one to smoke",
+    choices:["Pick either one because you don't want to seem scared","You Run away"],
     impact:[-5,3,1],
-    nextQuestion: [],
+    nextQuestion: [11,12,13],
+    //2c
   },
   {
-    picture:"",
-    question:"They pressure you to hit it",
-    choices:["tell them angirly to stop ",],
+    picture:"https://image.shutterstock.com/image-vector/upset-stick-figure-person-comforting-260nw-1580912602.jpg",
+    question:"They tell you that you're supposed to feel like that, it's fine.",
+    choices:["You feel really sick so you end up going home."],
+    impact:[0],
+    nextQuestion:[8],
+  },
+  {
+    picture:"https://cdn.glitch.com/6bec49b0-c740-4af8-809e-979aa297be0f%2FScreenshot%202020-02-07%20at%204.18.22%20PM.png?v=1581121113878",
+    question:"You become addicted to vaping",
+    choices:[],
     impact:[],
     nextQuestion:[],
   },
   {
     picture:"",
-    question:"",
-    choices:["",],
-    impact:[],
-    nextQuestion:[],
+    question:"After that you tell yourself you're never smoking again.",
+    choices:[],
+    impact:[0],
+    nextQuestion:[0],
   },
   {
     picture:"",
     question:"",
-    choices:["",],
-    impact:[],
-    nextQuestion:[],
-  },
-  {
-    picture:"",
-    question:"",
-    choices:["",],
+    choices:[],
     impact:[],
     nextQuestion:[],
   },
@@ -99,6 +103,12 @@ function displayQuestions(){
       $(".choiceB3").hide();
     if(data[index].choices.length < 3){
       $(".choiceB2").hide();
+      if(data[index].choices.length < 2){
+        $(".choiceB1").hide();
+          if(data[index].choices.length < 1){
+            $(".button").hide();
+        }
+      }
     }
   }
   
